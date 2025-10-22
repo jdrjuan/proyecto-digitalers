622be29e884b8bd2906de4293efb15b6825f2b61
Juan Romero <jdrjuan@gmail.com>
2025-10-22 19:57:58 -0300

    Utiliza las variables de .env desde config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
+import 'dotenv/config';
 
 const config = {
-    PORT: 8080,
-    PRODUCTS_FILE_NAME: 'products.data',
+    PORT: process.env.PORT || 8080,
+    PRODUCTS_FILE_NAME: process.env.PRODUCTS_FILE_NAME || 'products.data',
 };
 
 export default config;
